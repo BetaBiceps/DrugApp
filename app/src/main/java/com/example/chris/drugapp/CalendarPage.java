4acf4dd25a7df253c488d445de9833a44ca868f7
Chris <cdhunter94@gmail.com>
2016-01-28 20:04:13 -0800

    calendar added

--- app/src/main/java/com/example/chris/drugapp/CalendarPage.java
+++ app/src/main/java/com/example/chris/drugapp/CalendarPage.java
@@ -2,15 +2,26 @@ package com.example.chris.drugapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
 /**
  * Created by Chris on 27/01/2016.
  */
 public class CalendarPage extends Activity {
+
+    CalendarView calendarView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_layout);
 
+        calendarView = (CalendarView) findViewById(R.id.calendar);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener(){
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth){
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
