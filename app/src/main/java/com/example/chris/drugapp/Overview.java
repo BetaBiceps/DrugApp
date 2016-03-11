26df40ac6933d2c44d138040ed2de542746034a1
Chris <cdhunter94@gmail.com>
2016-03-11 13:39:58 -0800

    Overview updated, including graph

--- app/src/main/java/com/example/chris/drugapp/Overview.java
+++ app/src/main/java/com/example/chris/drugapp/Overview.java
@@ -1,32 +1,26 @@
 package com.example.chris.drugapp;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.os.Looper;
-import android.support.v4.app.NavUtils;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.BarGraphSeries;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
-import hirondelle.date4j.DateTime;
+import java.util.HashMap;
+
 
 /**
  * This is the main class for the overview page.
@@ -49,6 +43,10 @@ public class Overview extends Activity {
 
     OverviewAdapter listAdapter;
     ListView listView;
+    GraphView graphView;
+    BarGraphSeries<DataPoint> series;
+    LineGraphSeries<DataPoint> seriesLine;
+    DataPoint[] dataPoints;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -77,18 +75,55 @@ public class Overview extends Activity {
         month3.setText(Months[cal.get(Calendar.MONTH)]);
 
 
+        graphView = (GraphView) findViewById(R.id.graph);
+        fillPage();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        populateTotals();
+        fillPage();
+    }
+
+    protected  void fillPage(){
+        final String[] stringsOfDrugs = getResources().getStringArray(R.array.drugs);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String drugPicked = String.valueOf(listView.getItemAtPosition(position));
+                String drugPicked = stringsOfDrugs[position];
 
-                Toast.makeText(Overview.this, drugPicked, Toast.LENGTH_SHORT).show();
+                int val1 = getTotal(drugPicked, cal.get(Calendar.MONTH));
+                int val2 = getTotal(drugPicked, cal.get(Calendar.MONTH) - 1);
+                int val3 = getTotal(drugPicked, cal.get(Calendar.MONTH) - 2);
+
+                graphView.removeAllSeries();
+
+                //BAR GRAPH
+                series = new BarGraphSeries<DataPoint>(new DataPoint[]{
+                        new DataPoint(0, val3),
+                        new DataPoint(1, val2),
+                        new DataPoint(2, val1)
+                });
+
+
+                //LINE GRAPH
+                dataPoints = getDataPoints(drugPicked, cal.get(Calendar.MONTH));
+                seriesLine = new LineGraphSeries<DataPoint>(dataPoints);
+                seriesLine.setColor(Color.RED);
+
+                graphView.addSeries(series);
+                graphView.addSeries(seriesLine);
+
+                graphView.invalidate();
             }
         });
-
     }
 
+    /**
+     * Populates the totals for the overview page. Per month
+     */
     protected void populateTotals(){
         int total,total2, total3;
         cal.setTime(date);
@@ -131,6 +166,54 @@ public class Overview extends Activity {
         drugsArray.add(map2);
         drugsArray.add(map3);
 
+    }
+
+    /**
+     * Returns the total of a given drug useage in a month
+     * @param drug the drug to get totaled
+     * @param month the month to confine to
+     */
+    public int getTotal(String drug, int month){
+        int total = 0;
+        for(Event e : list){
+            cal.setTime(e.getDate());
+            int m = cal.get(Calendar.MONTH);
+
+            if(m == month){
+                if(e.getDrug().equals(drug)){
+                    total += e.getDose();
+                }
+            }
+        }
+        return total;
+    }
+
+    /**
+     * returns the data points for the past 3 months for a line grpah
+     * @param drug the drug to find
+     * @param month month to lookat
+     * @return data the datapoints
+     */
+    public DataPoint[] getDataPoints(String drug, int month){
+        ArrayList<DataPoint> data = new ArrayList<DataPoint>();
+        int i=0;
+
+        for(Event e : list){
+            cal.setTime(e.getDate());
+            int m = cal.get(Calendar.MONTH);
+
+            if(m == month || m+1 == month || m+2 == month){
+                if(e.getDrug().equals(drug)){
+                    data.add(new DataPoint(i,e.getDose()));
+                    i++;
+                }
+            }
+        }
+
+        DataPoint[] simpleArray = new DataPoint[data.size()];
+        data.toArray( simpleArray );
+
+        return simpleArray;
     }
 
 
