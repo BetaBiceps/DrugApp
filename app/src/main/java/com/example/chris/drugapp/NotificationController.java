f9b5cb19af4e37dc6995e78080fc83a73a6408da
Chris <cdhunter94@gmail.com>
2016-03-18 15:31:59 -0700

    all alerts for practicum complete

--- app/src/main/java/com/example/chris/drugapp/NotificationController.java
+++ app/src/main/java/com/example/chris/drugapp/NotificationController.java
@@ -13,6 +13,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * This class is responsible for determining when to alert users.
@@ -34,12 +35,16 @@ public class NotificationController extends BroadcastReceiver{
     final String freqIncreaseString = "Frequency of use is increasing, \ntry to dose less often.";
     final String doseHighString = "Dose is high. Use caution.";
     final String freqHighString = "Dosing again too soon. Use caution.";
-    final float avgIncreaseLimit = 0.1f; // 10% increase
-    final float avgFreqLimit = 0.5f; // 2 times as frequent
+    float avgIncreaseLimit = 0.15f; // 15% increase
+    float avgFreqLimit = 0.5f; // 2 times as frequent
     int requiredUseCount = 3; // No alerts unless there are enough events
     final int spamCountRequired = 3; // Controls repeating messages
+    final float doseLimitChangeIfMarked = 0.8f; // 80% dose limit if the drug is marked
+    final float freqLimitChangeIfMarked = 0.85f; // 85% dose limit if the drug is marked
+    final float avgIncreaseLimitChangedIfMarked = 0.01f; // changes limits by X if marked
     final HashMap<String,Integer> doseLimit = new HashMap<String, Integer>();//set in constructor
     final HashMap<String,Integer> freqLimit = new HashMap<String, Integer>();//set in constructor
+    HashMap<String,Boolean> isMarked = new HashMap<String, Boolean>();//set in constructor
 
     //TODO add a time frame limit, only look at doses in the past 1 or 2 months
 
@@ -54,27 +59,43 @@ public class NotificationController extends BroadcastReceiver{
     public NotificationController(Context context) {
         mContext = context;
         drugArray = mContext.getResources().getStringArray(R.array.drugs);
+        setDefault();
+    }
+
+    private void setDefault(){
+        doseLimit.clear();
         doseLimit.put("Meth", 50);
-        doseLimit.put("Ketamine",100);
+        doseLimit.put("Ketamine", 100);
         doseLimit.put("GHB",2000);
         doseLimit.put("Cocaine",500);
         doseLimit.put("Heroin",100);
         doseLimit.put("Marijuana",3000);
         doseLimit.put("MDMA",300);
 
+        freqLimit.clear();
         freqLimit.put("Meth", 7);
-        freqLimit.put("Ketamine",3);
+        freqLimit.put("Ketamine", 3);
         freqLimit.put("GHB",3);
         freqLimit.put("Cocaine",7);
         freqLimit.put("Heroin",5);
         freqLimit.put("Marijuana",1);
         freqLimit.put("MDMA",30);
+
+        isMarked.clear();
+        isMarked.put("Meth", false);
+        isMarked.put("Ketamine", false);
+        isMarked.put("GHB",false);
+        isMarked.put("Cocaine",false);
+        isMarked.put("Heroin",false);
+        isMarked.put("Marijuana",false);
+        isMarked.put("MDMA",false);
+
+        avgIncreaseLimit = 0.15f;
+        avgFreqLimit = 0.5f;
     }
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-
-    }
+    public void onReceive(Context context, Intent intent) {}
 
 
     /**
@@ -117,6 +138,8 @@ public class NotificationController extends BroadcastReceiver{
      * @param dose the dose of the drug
      */
     public void notifyCheck(ArrayList<Event> list, String drugAdded, int dose){
+        checkMarks(list);
+
         for (String drug : drugArray) {
             if (isDoseIncrease(list, drug)) {
                 createNotification(mContext, drug, doseIncreaseString, alertString);
@@ -132,6 +155,7 @@ public class NotificationController extends BroadcastReceiver{
         if(isFreqHigh(list, drugAdded)){
             createNotification(mContext, drugAdded, freqHighString, alertString);
         }
+
     }
 
     /**
@@ -170,6 +194,8 @@ public class NotificationController extends BroadcastReceiver{
                     counter >= (requiredUseCount)) {
                 requiredUseCount += spamCountRequired;
                 //If the avg increase is to much and the drug is used past the threshold, return true
+                isMarked.remove(drug);
+                isMarked.put(drug, true);
                 return true;
             }
         }
@@ -225,6 +251,8 @@ public class NotificationController extends BroadcastReceiver{
             if (lastFreq <= (avgFreqLimit * avgFreq) &&
                     counter >= (requiredUseCount)) {
                 requiredUseCount += spamCountRequired;
+                isMarked.remove(drug);
+                isMarked.put(drug, true);
                 return true;
             }
         }
@@ -240,6 +268,8 @@ public class NotificationController extends BroadcastReceiver{
      */
     private boolean isDoseHigh(String drug, int dose){
         if(dose >= doseLimit.get(drug)){
+            isMarked.remove(drug);
+            isMarked.put(drug, true);
             return true;
         }
         return false;
@@ -273,6 +303,42 @@ public class NotificationController extends BroadcastReceiver{
             return true;
         }
         return false;
+    }
+
+    /**
+     * Checks to see if any drug is marked for addiction case.
+     * If it is, we do a few things to increase the chance of notification.
+     * Clears to default every 10 entries
+     */
+    private void checkMarks(ArrayList<Event> list){
+        for(Map.Entry<String, Boolean> entry : isMarked.entrySet()){
+            String key = entry.getKey();
+            Boolean bool = entry.getValue();
+            int current=0;
+            int count=0;
+
+            //If the list size is a facotr of 10, we reset all settings.
+            if (list.size() % 10 == 0){
+                setDefault();
+                break;
+            }
+
+            //If a drug is marked we make more notifications.
+            if (bool == true){
+                count++;
+                current = doseLimit.get(key);
+                doseLimit.remove(key);
+                doseLimit.put(key, Math.round(current * doseLimitChangeIfMarked));
+
+                current = freqLimit.get(key);
+                freqLimit.remove(key);
+                freqLimit.put(key, Math.round(current * freqLimitChangeIfMarked));
+
+                avgIncreaseLimit -= avgIncreaseLimitChangedIfMarked;
+                avgFreqLimit += avgIncreaseLimitChangedIfMarked;
+            }
+        }
+
     }
 
 
